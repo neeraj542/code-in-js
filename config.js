82645c04e66dfeb3935d2c6c3ef32440c59107c7
Neeraj Meena <114648043+neeraj542@users.noreply.github.com>
2024-06-23 21:16:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const apiKey = "840f958562bdc18f1208d4368695e19";
+const apiKey = "840f958562bdc18f1208d4368695e19e";
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 
